f9afbaf8ffb7aba2cef6778ae661968124605053
Joshua Flynn <flynn4anything@hotmail.com>
2017-09-19 03:16:09 +0100

    Updated King-Pigeon.sh

--- Blueprints/King-Pigeon/King-Pigeon.sh
+++ Blueprints/King-Pigeon/King-Pigeon.sh
@@ -28,7 +28,7 @@ if [ "" == "$PKG_OK" ]; then
  	sudo apt-get --force-yes --yes install git
 fi
 
-#_user=$SUDO_USER
+#_user=SUDO_USER
 
 #Make our deployment directory
 #mkdir "/home/$_user/Modules"
@@ -37,14 +37,103 @@ fi
 #	exit 1;
 #fi
 
+#tell git to change it's directory (we want it to drop the module files in the right place for copying purposes)
+git -C "/home/$_user/"
+
 #Download the modules configuration system
 git clone https://github.com/JoshuaFlynn/Modules.git
+
+#Delete the garbage files off the desktop
+rm "/home/$_user/Desktop/_RELEASE_NOTES"
+rm "/home/$_user/Desktop/SMALLER FONTS.desktop"
+rm "/home/$_user/Desktop/LARGER FONTS.desktop"
 
 #Navigate to the new directory
 cd "/home/$_user/Modules"
 
 #Transfer a copy of the sources.list file (which should be correctly configured)
 cp /etc/apt/sources.list /etc/apt/sources.list.bak
-cp ./Blueprints/King-Pigeon/Replacements/sources.list /etc/apt
+cp "/home/$_user/Modules/Blueprints/King-Pigeon/Replacements/sources.list" /etc/apt
 
+#Now we've updated the sources.list file to include everything, update our own cache
+apt-get update
+
+#GIMP is a nice program, but to trim off the fat, it has to go
+apt-get purge -y gimp
+
+#Get rid of bloat office packages
+apt-get purge -y libreoffice libreoffice-base libreoffice-math libreoffice-calc libreoffice-writer libreoffice-bin libreoffice-draw libreoffice-impress
+
+#Get rid of the web browser
+apt-get purge -y firefox-esr iceweasel w3m
+
+#install a mini-web browser
+apt-get install -y midori
+
+#and printer support
+apt-get purge -y hplip
+
+#and more printer support
+apt-get purge -y cups cups-browsed cups-bsd cups-client cups-common cups-core-drivers cups-daemon cups-filters cups-filters-core-drivers cups-pk-helper cups-ppdc cups-server-common
+
+#and screen reader support
+apt-get purge -y gnome-orca
+
+#get rid of their xarchiver
+apt-get purge -y xarchiver xarchiver-debug
+
+#bring in our preferred file archiving program
+apt-get install -y file-roller
+
+#Get rid of evince (for now)
+apt-get purge -y evince-common
+
+#and get rid of their music support
+apt-get purge -y vlc
+
+#and CD burning capabilities
+apt-get purge -y xfburn
+
+#and scanning abilities
+apt-get purge -y xsane
+
+#Eww, Vim
+apt-get purge -y vim
+
+#Mousepad will be gone
+apt-get purge -y mousepad
+
+#Replace it with leafpad
+apt-get install -y leafpad
+
+#Get rid of audio garbage
+apt-get purge -y exfalso quodlibet
+
+#Get rid of XFCE entirely
+apt-get purge -y xfconf xfce4-utils xfwm4 xfce4-session thunar xfdesktop4 exo-utils xfce4-panel xfce4-terminal xfce4-taskmanager
+
+#Bring in LXDE
+apt-get install -y lxde-core
+
+#Get rid of the default display manager
+apt-get purge -y slim
+
+#Install the lightdm
+apt-get install -y lightdm
+
+#Finally, get rid of everything for realsies
+apt-get autoremove -y
+
+#Purge the usr/share folder containing the XFCE4 junk we no longer need, which isn't removed when XFCE goes
+rm -rf "/usr/share/xfce4"
+
+#Reinstall wicd, because for some reason this gets violently removed by one of the earlier libraries, but we actually need it
+apt-get install -y wicd
+
+apt-get install -y grub-efi-amd64
+
+#Update the .img file pre-emptively for the snapshot
 update-initramfs -u
+
+#Reboot so the changes take effect
+reboot
